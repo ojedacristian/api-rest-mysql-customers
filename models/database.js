e56c3ff01b1838db6ad9be8b29354f7287d5a49a
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2020-08-15 13:42:05 -0300

    Agregando promisify a las queries para usar promises en vez de callbacks

--- models/database.js
+++ models/database.js
@@ -1,13 +1,32 @@
 const mysql = require('mysql');
+const { promisify } = require('util');
 const dbconfig = require('../config/db.config.js');
 
 // Create a connection to the database
-const connection = mysql.createPool(dbconfig);
+const pool = mysql.createPool(dbconfig);
 
-// open the MySQL connection
-connection.getConnection((error) => {
-  if (error) throw error;
-  console.log('Successfully connected to the database.');
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.log(err.code);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database Connection was closed');
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('Database has to many connections');
+    }
+    if (err.code === 'ECONNREFUSED') {
+      console.error('DATABASE CONNECTION WAS REFUSED');
+    }
+  }
+
+  if (connection) {
+    connection.release();
+    console.log('Conexion a la BD realizada');
+  }
 });
 
-module.exports = connection;
+// Para poder usar promesas en vez de callbacks
+pool.query = promisify(pool.query);
+
+module.exports = pool;
